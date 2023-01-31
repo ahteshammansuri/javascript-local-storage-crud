d2241e8fd19a30ab45f5d93708436c8851254283
Ahtesham Mansuri <71078143+ahteshammansuri@users.noreply.github.com>
2023-01-31 15:37:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function () {
   // Validation Rules
   jQuery.validator.addMethod("lettersonly", function (value, element) {
-    return this.optional(element) || /^[a-z]+$/i.test(value);
-  }, "Only Letters Allowed");
+    return this.optional(element) || /^[a-zA-Z]+$/i.test(value);
+  }, "Please Enter Valid Title");
 
   jQuery("#addProductForm").validate({
     rules: {
@@ -16,12 +16,11 @@ jQuery(document).ready(function () {
       },
       productDescription: {
         required: true,
-        lettersonly: true
       },
       productCategory: {
         required: true,
       },
-      // "product[image]": {
+      // productImage: {
       //     required: true
       // },
     },
@@ -29,7 +28,7 @@ jQuery(document).ready(function () {
 
   getProductData();
   getCategoryData();
-  
+
   jQuery("#cancel").click(function () {
     afterUpdateChanges(this);
   });
@@ -41,34 +40,72 @@ jQuery(document).ready(function () {
     });
   }
 
-
+  var productImage = document.getElementById("productImage");
+  if (productImage != null && productImage) {
+    productImage.addEventListener('change', (event) => {
+      var image = event.target.files[0];
+      var reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.addEventListener('load', () => {
+        imagePath = reader.result;
+        document.getElementById("imagePath").src = reader.result;
+      });
+    });
+  }
+  setCategoryDataObj();
 });
 
 var productData = "productData";
+var categoryData = "categoryData";
 var storeArr = [];
 let id = "null";
+var imagePath = "";
+
+function setCategoryDataObj() {
+  var categoryArray = [];
+  var getCategoryData = document.getElementById("productCategory");
+  for (let i = 1; i < getCategoryData.length; i++) {
+    const element = getCategoryData.options[i].text;
+    getCategoryData.options[i].value = i;
+    categoryArray[i] = element;
+  }
+  localStorage.setItem(categoryData, JSON.stringify(categoryArray));
+}
+
+function getCategoryDataObj() {
+  return JSON.parse(localStorage.getItem(categoryData));
+}
 
 function getCategoryData() {
-  var productDataObj = getProductDataObj();
-  if (productDataObj) {
-    var categories = [];
-    productDataObj.forEach(function (arrayItem, key) {
-      categories.push(arrayItem.productCategory);
-    });
-    let uniqueCategories = categories.filter((item, i, ar) => ar.indexOf(item) === i);
+  var categoryDataObj = getCategoryDataObj();
+  if (categoryDataObj) {
     categoryTableData = "";
-    uniqueCategories.forEach(function (category) {
-      categoryTableData = categoryTableData +
-        "<tr><td>" + category +
-        "</td><td>" +
-        "<button id='viewProducts' class='btn btn-primary' onclick='productGridByCategory(\"" + category + "\")'>View</button></td></tr>";
-    });
+      for (let i = 1; i < categoryDataObj.length; i++) {
+        categoryTableData = categoryTableData +
+          "<tr><td>" + categoryDataObj[i] +
+          "</td><td>" +
+          "<button id='viewProducts' class='btn btn-primary' onclick='productGridByCategory(" + i + ")'>View</button></td></tr>";
+      }
     document.getElementById("categoryData").innerHTML = categoryTableData;
-  }else{
+  } else {
     document.getElementById("categoryData").innerHTML = "<td></td><td>No Records Found</td>";
   }
 }
 
+getCategoryNameById(1);
+getCategoryNameById(2);
+
+function getCategoryNameById(categoryId) {
+  var categoryDataObj = getCategoryDataObj();
+  // console.log(categoryId);
+  console.log(categoryDataObj[categoryId]);
+  return categoryDataObj[categoryId];
+  // category.forEach(element => {
+  //   if (element.id == categoryId) {
+  //     element.category;
+  //   }
+  // });
+}
 
 function notificationAutoClosed() {
   $("#alertNotification").fadeTo(2000, 500).slideUp(500, function () {
@@ -86,13 +123,13 @@ function afterUpdateChanges(element = null) {
   document.getElementById("productSave").setAttribute("style", "display: block");
   document.getElementById("update").setAttribute("style", "display: none");
   document.getElementById("cancel").setAttribute("style", "display: none");
+  document.getElementById("imagePath").src = "";
 }
 
-function productGridByCategory(category) {
+function productGridByCategory(categoryId) {
   document.getElementById("categoryGridData").setAttribute("style", "display: block;");
-  getProductData(category);
+  getProductData(categoryId);
 }
-
 
 function addProductData() {
   var formId = jQuery("#addProductForm");
@@ -100,12 +137,13 @@ function addProductData() {
     var productTitle = $("#productTitle")[0].value;
     var productPrice = $("#productPrice")[0].value;
     var productDescription = $("#productDescription")[0].value;
-    var productCategory = $("#productCategory")[0].value;
+    var productCategoryId = $("#productCategory")[0].value;
     var productObj = {
       productTitle,
       productPrice,
       productDescription,
-      productCategory,
+      productCategoryId,
+      imagePath
     };
     storeArr.push(productObj);
     setProductDataObj(storeArr);
@@ -127,34 +165,34 @@ function setProductDataObj(storeArr) {
   localStorage.setItem(productData, JSON.stringify(storeArr));
 }
 
-function getProductData(category = null) {
+function getProductData(categoryId = null) {
   var getProductObj = getProductDataObj();
   var productDataRow = document.getElementById("productDataRow");
   if (getProductObj != null) {
     var tableData = "";
     getProductObj.forEach(function (arrayItem, key) {
-      if (arrayItem.productCategory == category || category == null) {
+      if (arrayItem.productCategoryId == categoryId || categoryId == null) {
         tableData = tableData +
-          "<tr><td><img src='#' /></td><td>" +
+          "<tr><td><img src='" + arrayItem.imagePath + "' height='100' width='100' /></td><td>" +
           arrayItem.productTitle +
           "</td><td>" +
-          arrayItem.productCategory +
+          getCategoryNameById(arrayItem.productCategoryId) +
           "</td><td>" +
           arrayItem.productPrice;
-        if (category == null) {
+        if (categoryId == null) {
           tableData = tableData +
             "</td><td><a class='btn btn-info' href='#productAdd' onclick='editProduct(" + key + ")'>Edit</a> <button class='btn btn-danger' onclick='deleteProduct(" + key + ")'>Delete</button></td></tr>";
         }
       }
     });
-    if (category) {
+    if (categoryId) {
       document.getElementById("categoryProductDataRow").innerHTML = tableData;
       return true;
     }
     if (productDataRow) {
       productDataRow.innerHTML = tableData;
     }
-  }else{
+  } else {
     productDataRow.innerHTML = "<td></td><td></td><td>No Records Found</td>";
   }
 }
@@ -165,7 +203,9 @@ function editProduct(indexOf) {
   document.getElementById("productTitle").value = arr[indexOf].productTitle;
   document.getElementById("productPrice").value = arr[indexOf].productPrice;
   document.getElementById("productDescription").value = arr[indexOf].productDescription;
-  document.getElementById("productCategory").value = arr[indexOf].productCategory;
+  document.getElementById("productCategory").value = arr[indexOf].productCategoryId;
+  document.getElementById("imagePath").src = arr[indexOf].imagePath;
+  imagePath = arr[indexOf].imagePath;
   document.getElementById("cancel").setAttribute("style", "display: block");
   document.getElementById("productInfo").innerHTML = "Update Product";
   document.getElementById("update").setAttribute("style", "display: block");
@@ -173,22 +213,24 @@ function editProduct(indexOf) {
   document.getElementById("productGridData").setAttribute("style", "display: none");
   document.getElementById("addProductDiv").setAttribute("style", "display: block");
 }
+
 function updateProduct() {
   if (id != "null") {
     var formId = jQuery("#addProductForm");
     if (formId.valid()) {
-    let arr = getProductDataObj();
-    arr[id].productTitle = document.getElementById("productTitle").value;
-    arr[id].productPrice = document.getElementById("productPrice").value;
-    arr[id].productDescription = document.getElementById("productDescription").value;
-    arr[id].productCategory = document.getElementById("productCategory").value;
-    setProductDataObj(arr);
-    getProductData();
-    afterUpdateChanges();
-    document.getElementById("update").setAttribute("href", "#productGrid");
-    document.getElementById("alertNotification").setAttribute("style", "display: block");
-    document.getElementById("notificationText").innerHTML = "Product Updated successfully";
-    notificationAutoClosed();
+      let arr = getProductDataObj();
+      arr[id].productTitle = document.getElementById("productTitle").value;
+      arr[id].productPrice = document.getElementById("productPrice").value;
+      arr[id].productDescription = document.getElementById("productDescription").value;
+      arr[id].productCategoryId = document.getElementById("productCategory").value;
+      arr[id].imagePath = imagePath;
+      setProductDataObj(arr);
+      getProductData();
+      afterUpdateChanges();
+      document.getElementById("update").setAttribute("href", "#productGrid");
+      document.getElementById("alertNotification").setAttribute("style", "display: block");
+      document.getElementById("notificationText").innerHTML = "Product Updated successfully";
+      notificationAutoClosed();
     }
   }
 }
